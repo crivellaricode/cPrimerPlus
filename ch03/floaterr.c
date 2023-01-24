9c2a9163c947249d0cf192df7ef0f735062e48ac
Sidney Crivellari <crivellaricode@gmail.com>
2023-01-24 11:03:16 -0300

    floaterr.c

---
+++ ch03/floaterr.c
@@ -0,0 +1,15 @@
+/* floaterr.c -- demonstrate round-off error */
+
+#include <stdio.h>
+
+int main(void) 
+{   
+    float a, b;
+
+    b = 2.0e20 + 1;
+    a = b - 2.0e20;
+
+    printf("%f\n", a);
+        
+    return 0;    
+}
