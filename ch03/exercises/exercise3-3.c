ebb2ba7c65ae2b694deae0c2bc83a2621f979b40
Sidney Crivellari <crivellaricode@gmail.com>
2023-01-31 21:48:50 -0300

    exercise3-3.c

---
+++ ch03/exercises/exercise3-3.c
@@ -0,0 +1,14 @@
+/* exercise3-3.c */
+
+#include <stdio.h>
+
+int main(void)
+{
+    char bip = '\a';
+
+    printf("%c Startled by the sudden sound, Sally shouted,\n", bip);
+    printf("\"By the Great Pumpkin, what was that!\"\n");
+
+    return 0;
+}
+
